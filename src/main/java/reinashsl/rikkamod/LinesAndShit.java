6baf0a6527ec8a8b707fcfc565c94c0f3374729a
reina <ReinaSHSL@users.noreply.github.com>
2019-03-31 05:00:49 -0700

    positions

--- src/main/java/reinashsl/rikkamod/LinesAndShit.java
+++ src/main/java/reinashsl/rikkamod/LinesAndShit.java
@@ -3,6 +3,7 @@ package reinashsl.rikkamod;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.vfx.SpeechBubble;
 
@@ -15,7 +16,7 @@ public class LinesAndShit {
     )
     public static class Dmg {
         public static void Prefix(AbstractPlayer __instance, DamageInfo useless) {
-            AbstractDungeon.effectList.add(new SpeechBubble(Rikka.x + 50F, Rikka.y + 100F, "Daijoubu?", false));
+            AbstractDungeon.effectList.add(new SpeechBubble((Rikka.x + 200F) * Settings.scale, (Rikka.y + 300F) * Settings.scale, "Daijoubu?", true));
         }
     }
 
@@ -26,9 +27,9 @@ public class LinesAndShit {
     public static class StartTurn {
         public static void Prefix(AbstractPlayer __instance) {
             if (AbstractDungeon.miscRng.random(99) < 30) {
-                AbstractDungeon.effectList.add(new SpeechBubble(Rikka.x + 50F, Rikka.y + 100F, "Ganbarimasu!", false));
+                AbstractDungeon.effectList.add(new SpeechBubble((Rikka.x + 200F) * Settings.scale, (Rikka.y + 300F) * Settings.scale, "Ganbarimasu!", true));
             } else if (AbstractDungeon.miscRng.random(99) < 50) {
-                AbstractDungeon.effectList.add(new SpeechBubble(Rikka.x + 50F, Rikka.y + 100F, "Senpai!", false));
+                AbstractDungeon.effectList.add(new SpeechBubble((Rikka.x + 200F) * Settings.scale, (Rikka.y + 300F) * Settings.scale, "Senpai!", true));
             }
         }
     }
